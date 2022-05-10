23488495efb5cc32dc41dc43c8776833b0e10ef9
Mariel <marielvaca@gmail.com>
2022-05-09 20:02:05 -0500

    added cookie; phase 0 complete

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 //test route
-router.get('/hello/world', function(req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
+// router.get('/hello/world', function(req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
+
+//setting cookie on the response with name XSRF-Token
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
 });
+
+
+
+
 module.exports = router;
