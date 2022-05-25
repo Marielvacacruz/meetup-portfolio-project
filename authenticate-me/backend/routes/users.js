63883f59f7a196de4010837872bf1592d91c2921
Mariel <marielvaca@gmail.com>
2022-05-24 21:04:24 -0500

    get all groups joined or organized by user

--- authenticate-me/backend/routes/users.js
+++ authenticate-me/backend/routes/users.js
@@ -1,6 +1,6 @@
 const express =  require('express');
-const { setTokenCookie, restoreUser } = require('../utils/auth');
-const { User } = require('../db/models');
+const { setTokenCookie, restoreUser, requireAuth } = require('../utils/auth');
+const { User, Group, Member, Image, sequelize} = require('../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../utils/validation');
 
@@ -51,6 +51,46 @@ router.get('/current', restoreUser, (req, res) => {
   } else return res.json({});
 });
 
+//Get all Groups Joined or Organized by Current User
+router.get('/current/groups', requireAuth, async (req, res) => {
+  const { user } = req;
+
+  const Groups = await Group.findAll({
+    where:{
+     organizerId: user.id
+  },
+  include: [
+    {
+      model: Member,
+      attributes: [],
+      where: {userId: user.id}
+    },
+    {
+      model: Image,
+      attributes: []
+  }
+  ],
+  attributes: [
+    'id',
+    'organizerId',
+    "name",
+    "about",
+    "type",
+    "private",
+    "city",
+    "state",
+    "createdAt",
+    "updatedAt",
+    [sequelize.fn("COUNT", sequelize.col("Members.id")), "numMembers"], //numMembers count is wrong
+    [sequelize.col('Images.url'), 'previewImage']
+  ],
+group: ["Group.id"]
+});
+
+  return res.json({
+    Groups
+  })
+});
 
 
 module.exports =  router;
